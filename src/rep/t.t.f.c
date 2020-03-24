126fa6a925941b9ce2fc4c8500bcaa212080ae3c
Arthur Krause <amkrause@inf.ufrgs.br>
2020-03-24 18:12:17 -0300

    mais testes

---
+++ src/rep/t.t.f.c
@@ -0,0 +1,34 @@
+#include <immintrin.h>
+#include <stdint.h>
+
+#define ARRAY_SIZE 1024*256
+#define REP 1000
+int main(int argc, char **argv){
+	static	__m512i mem[ARRAY_SIZE];
+	__m512i local;
+	
+	unsigned long long int acc = 0;
+
+    for(int j=0; j<REP; j++){ 	
+	for(int i=0; i<ARRAY_SIZE; i++)
+	{
+		__m512i temp;
+		temp[0]=i;
+		_mm512_store_si512(&mem[i], temp);
+	}
+	_mm_mfence();
+	_wbinvd();
+	_mm_mfence();
+
+	for(int i=0; i < ARRAY_SIZE; i++)
+	{
+		local = _mm512_load_si512(&mem[i]);
+		acc += *((uint64_t*)&local);
+	}
+	_mm_mfence();
+
+	_wbinvd();
+	_mm_mfence();
+    }
+	return (int)acc;
+}
