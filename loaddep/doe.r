471b3cb1290ca05d217834c45399c67c5986677b
Arthur M. Krause <amkrause@inf.ufrgs.br>
2020-06-28 17:47:44 -0300

    doe loaddep

--- loaddep/doe.r
+++ loaddep/doe.r
@@ -2,16 +2,18 @@ require(DoE.base);
 #library(DoE.wrapper);
 #library(dplyr)
 scenario <- fac.design (
-         nfactors=2,
-         replications=50,
+         nfactors=4,
+         replications=30,
          repeat.only=FALSE,
          blocks=1,
          randomize=TRUE,
          seed=10373,
-         nlevels=c(length(seq(0,14)),3),
+         nlevels=c(length(seq(0,9)),3,2,2),
          factor.names=list(
-              size=seq(0,14),
-              temp=c("n","t","e")
+              size=seq(0,9),
+              temp=c("n","t","e"),
+			  mem=c("wb","wc"),
+			  shuff=c("seq","shuffle")
          )) ;
 
 export.design(scenario,
